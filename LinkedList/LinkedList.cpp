a478fa17458b276e7d893a4e4689c6abfa45232f
ELMA <ELMA@ELMA>
2020-09-26 14:24:07 +0800

    Main函数未实现

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -1,18 +1,18 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS 1   //Visual Studio Scanf会被定义为不安全操作导致无法编译故将安全警告关闭
+#define Size 1000
 #include <stdio.h>
 #include <stdlib.h>
-#define Size 1000
 typedef struct LinkList
 //创造链表结构体
 {
 	int elem;
-	struct LinkList *next;
+	struct LinkList* next;
 }LinkList;
-LinkList *initLink()
+LinkList* initLink()
 //建立链表
 {
-	LinkList *p = (LinkList*)malloc(sizeof(LinkList));//头节点
-	LinkList *temp = p;//指向头节点的指针
+	LinkList* p = (LinkList*)malloc(sizeof(LinkList));//头节点
+	LinkList* temp = p;//指向头节点的指针
 	for (int i = 1; i < Size; i++)
 	{
 		LinkList* a = (LinkList*)malloc(sizeof(LinkList));
@@ -23,10 +23,10 @@ LinkList *initLink()
 	}
 	return p;
 }
-LinkList* insertElem(LinkList *t, int elem, int position)
+LinkList* insertElem(LinkList* t, int elem, int position)
 //实现数据插入链表
 {
-	LinkList *temp = t;
+	LinkList* temp = t;
 	for (int i = 1; i < position ; i++)
 	{
 		temp = temp->next;
@@ -45,6 +45,59 @@ LinkList* insertElem(LinkList *t, int elem, int position)
 }
 LinkList* DeleteElem(LinkList* t, int position)
 //删除相应位置的元素
+{
+	LinkList* temp = t;
+	for (int i = 1; i < position; i++)
+	//寻找应删除的节点上一个节点
+	{
+		temp = temp->next;
+		if(temp->next==NULL)
+		{
+			printf("不存在该位置的节点");
+			return t;
+		}
+	}
+	LinkList* del = temp->next;
+	temp->next = del->next;
+	free(del);
+	return t;
+}
+LinkList* ChangeElem(LinkList* t, int elem, int position)
+//修改节点
+{
+	LinkList* temp = t;
+	temp = temp->next;
+	for (int i = 1; i < position; i++)
+	{
+		temp = temp->next;
+	}
+	temp->elem = elem;
+	return t;
+}
+int SearchElem(LinkList* t, int elem)
+//寻找值为elem的节点
+{
+	LinkList* temp = t;
+	int position;
+	while (temp->next)
+	{
+		temp = temp->next;
+		if (temp->elem == elem)return position;
+		position++;
+	}
+	return -1;
+}
+void display(LinkList* t)
+{
+	LinkList* temp = t;
+	while (temp->next)
+	{
+		temp = temp->next;
+		printf("%d", temp->elem);
+	}
+	printf("\n");
+}
+int main()
 {
 	
 }
--- SqList/SqList.cpp
+++ SqList/SqList.cpp
@@ -1,134 +1,99 @@
-﻿// SqList.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+﻿// Sqlist.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 #define _CRT_SECURE_NO_WARNINGS 1   //Visual Studio Scanf会被定义为不安全操作导致无法编译故将安全警告关闭
 #include <stdio.h>
 #include <stdlib.h>
-#define Size 10000
-typedef struct mylist
+#define Size 1000
+typedef struct Sqlist
 {
-
     int* head;
-    int len;
-    int size;
-}mylist;
-mylist Initmylist()
-//构造一个空的线性表
+    int length;
+    int listsize;
+}Sqlist;
+Sqlist EmptyList()
+//构造空表
 {
-    mylist t;
+    Sqlist t;
     t.head = (int*)malloc(Size * sizeof(int));
     if (!t.head)
     {
-        printf("初始化失败\n");
+        printf("初始化失败请重试\n");
         exit(0);
     }
-    t.len = 0;
-    t.size = Size;
+    t.length = 0;
+    t.listsize = Size;
     return t;
 }
-mylist addmylist(mylist t, int elem, int add)
-//在顺序线性表t中第add位置之前插入新的元素elem
+Sqlist Add(Sqlist t, int elem, int add)
+//插入输入的元素
 {
-    if (add >= t.len || add < 0)
-    {
-        puts("插入位置不合法");
-        return t;
-    }
-    if (t.len == t.size)
-    {
-        t.head = (int*)realloc(t.head, (t.size + 1) * sizeof(int));
-        if (!t.head)
-        {
-            puts("存储分配失败");
-            exit(0);
-        }
-        t.size++;
-    }
-    for (int i = t.len - 1;i >= add;i--)  t.head[i + 1] = t.head[i];
+    for (int i = t.length - 1; i >= add; i--)  t.head[i + 1] = t.head[i];
     t.head[add] = elem;
-    t.len++;
+    t.length++;
     return t;
 }
-mylist delmylist(mylist t, int add)
-//在顺序线性表t中删除第add个位置的元素
+Sqlist deleteEle(Sqlist t, int position)
+//删除相应位置的元素
 {
-    if (add >= t.len || add < 0)
-    {
-        printf("被删除元素的位置非法\n");
-        return t;
-    }
-    for (int i = add + 1;i < t.len;i++) t.head[i - 1] = t.head[i];
-    t.len--;
+    for (int i = position + 1; i < t.length; i++) t.head[i - 1] = t.head[i];
+    t.length--;
     return t;
 }
-int selectmylist(mylist t, int elem)
-//搜索第一个值为elem的元素，并返回它的位置
+int searchfor(Sqlist t, int elem)
+//搜索第一个值为elem的元素返回它的位置
 {
-    for (int i = 0;i < t.len;i++)
+    for (int i = 0; i < t.length; i++)
         if (t.head[i] == elem)
             return i;
     return -1;
 }
-mylist amendmylist(mylist t, int elem, int newElem)
+Sqlist change(Sqlist t, int elem, int newElem)
 //更改函数，其中，elem为要更改的元素的值，newElem为新的数据元素
 {
-    int add = selectmylist(t, elem);
+    int add = searchfor(t, elem);
     t.head[add] = newElem;
     return t;
 }
-void displaymylist(mylist t)
+void display(Sqlist t)
 //输出顺序表
 {
-    for (int i = 0;i < t.len;i++) printf("%d ", t.head[i]);
+    for (int i = 0; i < t.length; i++) printf("%d ", t.head[i]);
     puts("");
 }
 int main()
 {
-    mylist t1 = Initmylist();
-    puts("请输入顺序表内元素的个数:");
-    scanf("%d", &t1.len);
-    puts("请依次输入这些元素");
-    for (int i = 0;i < t1.len;i++) scanf("%d", &t1.head[i]);
+    Sqlist L = EmptyList();
+    puts("请输入元素个数:");
+    scanf("%d", &L.length);
+    puts("请输入这些元素");
+    for (int i = 0; i < L.length; i++) scanf("%d", &L.head[i]);
     printf("原顺序表：\n");
-    displaymylist(t1);
-    printf("请选择你要实现的功能\n1.删除顺序表中值为key的所有结点\n2.删除顺序表中值重复的结点\n");
+    display(L);
+    printf("请选择功能\n1.删除顺序表中值为key的所有结点\n2.删除顺序表中值重复的结点\n");
     int choice;
     scanf("%d", &choice);
+    while (choice != 1 && choice != 2) {
+        puts("输入错误请重新输入选项");
+        scanf("%d", &choice);
+    }
     if (choice == 1)
     {
         int key;
-        printf("请输入要删除的key值:\n");
+        printf("请输入key值:\n");
         scanf("%d", &key);
         while (1)
         {
-            int now = selectmylist(t1, key);
+            int now = searchfor(L, key);
             if (now == -1) break;
-            t1 = delmylist(t1, now);
+            L = deleteEle(L, now);
         }
     }
     else if (choice == 2)
     {
-        for (int i = 0;i < t1.len;i++)
-            for (int j = i + 1;j < t1.len;j++)
-                if (t1.head[i] == t1.head[j])
-                    t1 = delmylist(t1, i--);
+        for (int i = 0; i < L.length; i++)
+            for (int j = i + 1; j < L.length; j++)
+                if (L.head[i] == L.head[j])
+                    L = deleteEle(L, i--);
     }
-    else
-    {
-        puts("输入错误");
-        exit(0);
-    }
-
-    displaymylist(t1);
+    display(L);
     return 0;
 }
-
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
